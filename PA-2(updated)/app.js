a717ca3e060a18c691f5fd931aaa9491fabdec04
ianpioquinto <65481086+ianpioquinto@users.noreply.github.com>
2021-02-11 17:09:08 +0800

    second commit
    updated with localhost server

---
+++ PA-2(updated)/app.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    res.setHeader('Content-Type', 'text/html');
+    let urll = './views/';
+
+    if(req.url == '/'){
+        urll += 'index.html';
+        res.statusCode == 200;
+    }
+    else if(req.url == '/about'){
+        urll += 'about.html';
+        res.statusCode == 200;
+    }
+    else if(req.url == '/contact'){
+        urll += 'contact.html';
+        res.statusCode == 200;
+    }
+    else{
+        urll += '404.html';
+        res.statusCode == 404;
+    }
+
+    fs.readFile(urll,(err, data) =>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.end(data);
+        }
+    });
+});
+
+server.listen(8082, 'localhost', () =>{
+    console.log('listening');
+});
